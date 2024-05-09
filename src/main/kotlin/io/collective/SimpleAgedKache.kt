fb1cec3c3cab5b06fd4c6993c969d2b1996d84c3
Trevor Stahl <trevor@trevorstahl.com>
2024-05-09 15:29:59 -0500

    rewrite in kotlin, tests pass

--- src/main/kotlin/io/collective/SimpleAgedKache.kt
+++ src/main/kotlin/io/collective/SimpleAgedKache.kt
@@ -1,27 +1,83 @@
 package io.collective
 
 import java.time.Clock
+import java.time.Instant
 
-class SimpleAgedKache {
-    constructor(clock: Clock?) {
-    }
+class SimpleAgedKache(private val clock: Clock = Clock.systemUTC()) {
+    private var newestEntry: ExpirableEntryLinkedList? = null
+    private var count = 0
 
-    constructor() {
-    }
+    inner class ExpirableEntryLinkedList(
+        val key: Any,
+        val value: Any,
+        val expirationTime: Instant,
+        var next: ExpirableEntryLinkedList? = null
+    )
 
-    fun put(key: Any?, value: Any?, retentionInMillis: Int) {
+    fun put(key: Any, value: Any, retentionInMillis: Int) {
+        val expirationTime = clock.instant().plusMillis(retentionInMillis.toLong())
+        val newEntry = ExpirableEntryLinkedList(key, value, expirationTime)
 
-    }
-
-    fun isEmpty(): Boolean {
-        return false
+        newEntry.next = newestEntry
+        newestEntry = newEntry
     }
 
     fun size(): Int {
-        return 0
+        val rightNow = clock.instant()
+        count = 0
+
+        if (newestEntry == null || newestEntry!!.expirationTime.isBefore(rightNow)) {
+            newestEntry = null
+            return count
+        }
+
+        var iterEntry = newestEntry
+        var prevEntry: ExpirableEntryLinkedList? = null
+
+        while (iterEntry != null) {
+            if (iterEntry.expirationTime.isAfter(rightNow)) {
+                count++
+                prevEntry = iterEntry
+                iterEntry = iterEntry.next
+            } else {
+                if (prevEntry != null) {
+                    prevEntry.next = null
+                }
+                break
+            }
+        }
+
+        return count
     }
 
-    fun get(key: Any?): Any? {
+    fun isEmpty(): Boolean = size() == 0
+
+    fun get(key: Any): Any? {
+        val rightNow = clock.instant()
+
+        if (newestEntry == null || newestEntry!!.expirationTime.isBefore(rightNow)) {
+            newestEntry = null
+            return null
+        }
+
+        var iterEntry = newestEntry
+        var prevEntry: ExpirableEntryLinkedList? = null
+
+        while (iterEntry != null) {
+            if (iterEntry.key == key) {
+                if (iterEntry.expirationTime.isAfter(rightNow)) {
+                    return iterEntry.value
+                } else {
+                    if (prevEntry != null) {
+                        prevEntry.next = null
+                    }
+                    break
+                }
+            }
+            prevEntry = iterEntry
+            iterEntry = iterEntry.next
+        }
+
         return null
     }
 }
